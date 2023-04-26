01ff7be1dd5cfdaaca614eefe0087b8cd8e69e01
Yevhen Larin <minard1usz@gmail.com>
2023-04-26 19:28:11 +0300

    added half completed main page

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items : 1, 
+      itemsDesktop : false,
+      itemsDesktopSmall : false,
+      itemsTablet: false,
+      itemsMobile : false,
+        margin: 20,
+        center: true,
+        loop: true
+    });
+  });
